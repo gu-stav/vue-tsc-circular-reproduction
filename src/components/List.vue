d2473f7d0565b792648259df43909a61ff721bca
Gustav Hansen <hansen@wienfluss.net>
2024-12-17 10:12:40 +0100

    wip

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { computed, defineEmits } from 'vue'
+import { defineEmits, ref } from 'vue'
 
 import { CollapsibleRoot } from 'radix-vue'
 
 import Sublist from './Sublist.vue'
 
 const emit = defineEmits(['branch:click'])
-const isOpen = computed(() => true)
+const isOpen = ref(true)
 </script>
 
 <template>
